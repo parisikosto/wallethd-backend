8b09a01ff2b570e7eec5dff8f1cec2e3e1b4bae4
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-01 19:20:14 +0200

    feat(auth): add auth basic controllers and routes

---
+++ src/routes/v1/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const { register, login, getProfile } = require("../../controllers/auth");
+
+const { authenticate } = require("../../middleware/auth");
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", authenticate, getProfile);
+
+module.exports = router;
