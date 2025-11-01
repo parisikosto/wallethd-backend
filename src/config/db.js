7a3e85acde4796c5ec8cbd89682598e1ca02d255
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-01 18:11:52 +0200

    refactor(*): change tab size to 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-        autoIndex: process.env.NODE_ENV !== "production",
-    });
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    autoIndex: process.env.NODE_ENV !== "production",
+  });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.green.underline);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.green.underline);
 };
 
 module.exports = connectDB;
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-    port: process.env.SERVER_PORT || "5000",
+  port: process.env.SERVER_PORT || "5000",
 };
 
 module.exports = config;
