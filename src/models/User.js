d071a93fda91411b4c82e028ab983e17ab57c640
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-01 18:13:35 +0200

    feat(user): add User model

---
+++ src/models/User.js
@@ -0,0 +1,82 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please add a username"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      maxlength: [50, "Username can not be more than 50 characters"],
+      match: [
+        /^[a-z0-9]+$/,
+        "Username can only contain lowercase letters and numbers",
+      ],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+    },
+    firstName: String,
+    lastName: String,
+    profileImage: {
+      type: String,
+      default: "no-profile-photo.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// lowercase usernames
+UserSchema.pre("save", function (next) {
+  if (this.isModified("username")) {
+    this.username = this.username.toLowerCase();
+  }
+
+  next();
+});
+
+// sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema, "users");
