e5d6cc8dc7ef1f8170fb03d3b7c239af2c5a82af
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-09 11:45:27 +0200

    feat(accounts): add seed data

--- _data/seeder.js
+++ _data/seeder.js
@@ -9,6 +9,7 @@ require('colors');
 const mongoose = require('mongoose');
 
 // load models
+const Account = require('../src/models/Account');
 const Category = require('../src/models/Category');
 const Settings = require('../src/models/Settings');
 const User = require('../src/models/User');
@@ -17,6 +18,9 @@ const User = require('../src/models/User');
 mongoose.connect(process.env.MONGO_URI);
 
 // read JSON files
+const accounts = JSON.parse(
+  fs.readFileSync(`${__dirname}/accounts.json`, 'utf-8'),
+);
 const categories = JSON.parse(
   fs.readFileSync(`${__dirname}/categories.json`, 'utf-8'),
 );
@@ -28,6 +32,7 @@ const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 // import into DB
 const importData = async () => {
   try {
+    await Account.insertMany(accounts);
     await Category.insertMany(categories);
     await Settings.create(settings);
     await User.create(users);
@@ -42,6 +47,7 @@ const importData = async () => {
 // delete data
 const deleteData = async () => {
   try {
+    await Account.deleteMany();
     await Category.deleteMany();
     await Settings.deleteMany();
     await User.deleteMany();
