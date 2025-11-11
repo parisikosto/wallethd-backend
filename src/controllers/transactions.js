bed59f6fabf68ccdf2b654067360497802bf8395
Paris Kostopoulos <parisikosto@gmail.com>
2025-11-11 18:59:46 +0200

    feat(transactions): add the rest of controllers and routes

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,5 +1,6 @@
 const { asyncHandler } = require('../middleware/asyncHandler');
 const Transaction = require('../models/Transaction');
+const { ErrorResponse } = require('../utils/errorResponse');
 
 /**
  * @desc    Get transactions
@@ -8,6 +9,26 @@ const Transaction = require('../models/Transaction');
  */
 const getTransactions = asyncHandler(async (_, res) => {
   res.status(200).json(res.advancedResults);
+});
+
+/**
+ * @desc    Get single transaction
+ * @route   GET /v1/transactions/:id
+ * @access  Private
+ */
+const getSingleTransaction = asyncHandler(async (req, res, next) => {
+  const transaction = await Transaction.findOne({
+    _id: req.params.id,
+    user: req.user.id,
+  });
+
+  if (!transaction) {
+    return next(
+      new ErrorResponse(`Transaction with id ${req.params.id} not found`, 404),
+    );
+  }
+
+  res.status(200).json({ success: true, data: transaction });
 });
 
 /**
@@ -21,4 +42,77 @@ const createTransaction = asyncHandler(async (req, res) => {
   res.status(201).json({ success: true, data: transaction });
 });
 
-module.exports = { getTransactions, createTransaction };
+/**
+ * @desc    Update transaction
+ * @route   PUT /v1/transactions/:id
+ * @access  Private
+ */
+const updateTransaction = asyncHandler(async (req, res, next) => {
+  const transaction = await Transaction.findOne({
+    _id: req.params.id,
+    user: req.user.id,
+  });
+
+  if (!transaction) {
+    return next(
+      new ErrorResponse(`Transaction with id ${req.params.id} not found`, 404),
+    );
+  }
+
+  const updatableFields = [
+    'type',
+    'date',
+    'dueDate',
+    'reminderDate',
+    'amount',
+    'status',
+    'note',
+    'facility',
+    'description',
+    'receiptTaken',
+    'isInstallment',
+    'isReadyToDeduct',
+    'attachments',
+    'website',
+    'account',
+    'category',
+  ];
+
+  updatableFields.forEach((field) => {
+    if (req.body[field] !== undefined) {
+      transaction[field] = req.body[field];
+    }
+  });
+
+  await transaction.save();
+
+  res.status(200).json({ success: true, data: transaction });
+});
+
+/**
+ * @desc    Delete transaction
+ * @route   DELETE /v1/transactions/:id
+ * @access  Private
+ */
+const deleteTransaction = asyncHandler(async (req, res, next) => {
+  const transaction = await Transaction.findOneAndDelete({
+    _id: req.params.id,
+    user: req.user.id,
+  });
+
+  if (!transaction) {
+    return next(
+      new ErrorResponse(`Transaction with id ${req.params.id} not found`, 404),
+    );
+  }
+
+  res.status(200).json({ success: true, data: {} });
+});
+
+module.exports = {
+  getTransactions,
+  getSingleTransaction,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
+};
--- src/routes/v1/transactions.js
+++ src/routes/v1/transactions.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const {
   getTransactions,
+  getSingleTransaction,
   createTransaction,
+  updateTransaction,
+  deleteTransaction,
 } = require('../../controllers/transactions');
 const { privateAdvancedResults } = require('../../middleware/advancedResults');
 const { authenticate } = require('../../middleware/auth');
@@ -16,5 +19,9 @@ router
   .route('/')
   .get(privateAdvancedResults(Transaction), getTransactions)
   .post(createTransaction);
+
+router.route('/:id').get(getSingleTransaction).put(updateTransaction);
+
+router.route('/:id').delete(deleteTransaction);
 
 module.exports = router;
