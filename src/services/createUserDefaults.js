a3da91de4bbca977cb3f5a42d801b846e2df7613
Paris Kostopoulos <parisikosto@gmail.com>
2026-05-19 22:44:36 +0300

    refactor(createUserDefaults): streamline account and category creation logic to prevent reseeding

--- src/services/createUserDefaults.js
+++ src/services/createUserDefaults.js
@@ -11,68 +11,61 @@ const createUserDefaults = async (userId) => {
    * create user default accounts
    */
   const existingAccounts = await Account.countDocuments({ user: userId });
-  if (existingAccounts > 0) {
-    return;
+  if (existingAccounts === 0) {
+    const defaultAccounts = defaultAccountsNames.map((name, idx) => ({
+      user: userId,
+      name,
+      order: idx,
+    }));
+
+    await Account.insertMany(defaultAccounts);
   }
-
-  const defaultAccounts = defaultAccountsNames.map((name, idx) => ({
-    user: userId,
-    name,
-    order: idx,
-  }));
-
-  await Account.insertMany(defaultAccounts);
 
   /**
    * create user default categories and subcategories
    */
-  // avoid reseeding if user already has categories
   const existingCategories = await Category.countDocuments({ user: userId });
-  if (existingCategories > 0) {
-    return;
-  }
-
-  // add main categories
-  const mainCategoriesDocs = mainCategories.map(
-    ({ name, transactionType, description, order }) => ({
-      user: userId,
-      name,
-      transactionType,
-      description: description || '',
-      order,
-      slug: slugify(name, {
-        lower: true,
-        strict: true,
-        trim: true,
+  if (existingCategories === 0) {
+    const mainCategoriesDocs = mainCategories.map(
+      ({ name, transactionType, description, order }) => ({
+        user: userId,
+        name,
+        transactionType,
+        description: description || '',
+        order,
+        slug: slugify(name, {
+          lower: true,
+          strict: true,
+          trim: true,
+        }),
       }),
-    }),
-  );
+    );
 
-  const createdMainCategories = await Category.insertMany(mainCategoriesDocs);
+    const createdMainCategories = await Category.insertMany(mainCategoriesDocs);
 
-  const keyToId = {};
-  for (let i = 0; i < mainCategories.length; i++) {
-    keyToId[mainCategories[i].key] = createdMainCategories[i]._id;
-  }
+    const keyToId = {};
+    for (let i = 0; i < mainCategories.length; i++) {
+      keyToId[mainCategories[i].key] = createdMainCategories[i]._id;
+    }
 
-  // add subcategories
-  const subCategoriesDocs = subCategories.map(
-    ({ name, transactionType, description, order, parentKey }) => ({
-      user: userId,
-      name,
-      transactionType,
-      parent: keyToId[parentKey],
-      description: description || '',
-      order,
-      slug: slugify(name, {
-        lower: true,
-        strict: true,
-        trim: true,
+    const subCategoriesDocs = subCategories.map(
+      ({ name, transactionType, description, order, parentKey }) => ({
+        user: userId,
+        name,
+        transactionType,
+        parent: keyToId[parentKey],
+        description: description || '',
+        order,
+        slug: slugify(name, {
+          lower: true,
+          strict: true,
+          trim: true,
+        }),
       }),
-    }),
-  );
+    );
 
-  await Category.insertMany(subCategoriesDocs);
+    await Category.insertMany(subCategoriesDocs);
+  }
 
   /**
    * create user default settings
